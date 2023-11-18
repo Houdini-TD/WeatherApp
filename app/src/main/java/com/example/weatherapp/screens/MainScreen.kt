2ad9ff9f44d277eabd37c6c1aee6f48c5f86f896
Houdini <davletshin2004@mail.ru>
2023-11-18 21:29:42 +0300

    Modified Weather card

--- app/src/main/java/com/example/weatherapp/screens/MainScreen.kt
+++ app/src/main/java/com/example/weatherapp/screens/MainScreen.kt
@@ -4,25 +4,25 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.blur
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -47,7 +47,7 @@ fun MainScreen() {
     ){
         Card(modifier = Modifier
             .fillMaxWidth()
-            .fillMaxHeight(0.3f),
+            .wrapContentHeight(),
             colors = CardDefaults.cardColors(
                 containerColor = PurpleGrey80,
                 contentColor = Color.Gray),
@@ -55,7 +55,9 @@ fun MainScreen() {
             shape = RoundedCornerShape(10.dp)
         ){
             Column(
-                modifier = Modifier.fillMaxWidth(),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(14.dp),
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
                 Row(modifier = Modifier.fillMaxWidth(),
@@ -63,14 +65,53 @@ fun MainScreen() {
                 ){
                     Text(
                         modifier = Modifier.padding(5.dp),
-                        text = "20 jun 2022 dsadsadsad20:30",
+                        text = "20 jun 2022 20:30",
                         style = TextStyle(fontSize = 18.sp),
                         color = Color.Black
                     )
                     AsyncImage(model = "https://cdn.weatherapi.com/weather/64x64/night/143.png",
                         contentDescription = "im2",
-                        modifier = Modifier.size(35.dp)
-                            .padding(5.dp));
+                        modifier = Modifier.size(35.dp));
+                }
+                Column(horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(text = "Санкт-Петербург",
+                        fontSize = 24.sp,
+                        color = Color.DarkGray)
+                    Text(text = "9°С",
+                        style = TextStyle(fontSize = 72.sp,
+                            color = colorResource(R.color.black),
+                            fontWeight = FontWeight.Medium)
+                    )
+                    Text(text = "Переменная облачность",
+                        style = TextStyle(
+                            fontSize = 18.sp,
+                            color = Color.DarkGray
+                        )
+                    )
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    ){
+                        IconButton(onClick = { /*TODO*/ }) {
+                            Icon(painter = painterResource(id = R.drawable.search),
+                                contentDescription = "search",
+                                modifier = Modifier.fillMaxSize(),
+                                tint = Color.DarkGray)
+                        }
+                        Text(text = "24/7",
+                            style = TextStyle(
+                                fontSize = 18.sp,
+                                color = Color.DarkGray
+                            )
+                        )
+                        IconButton(onClick = { /*TODO*/ }) {
+                            Icon(painter = painterResource(id = R.drawable.refresh),
+                                contentDescription = "refresh",
+                                tint = Color.DarkGray,
+                                modifier = Modifier.fillMaxSize())
+                        }
+                    }
                 }
 
             }
